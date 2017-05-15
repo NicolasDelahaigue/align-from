eefc6a489c746e89df92cda4bbc0071b435c81c1
Nicolas Delahaigue <nicolas.delahaigue@epitech.eu>
2017-05-16 00:43:36 +0300

    First version

--- src/extension.ts
+++ src/extension.ts
@@ -7,18 +7,29 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "align-from" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+        vscode.commands.executeCommand('actions.find', 'from').then(() => {
+            vscode.commands.executeCommand('editor.action.selectAllMatches').then(() => {
+                vscode.commands.executeCommand('cursorLeft').then(() => {
+                    vscode.commands.executeCommand('extension.align').then(() => {
+                        vscode.commands.executeCommand('closeFindWidget');
+                        vscode.commands.executeCommand('removeSecondaryCursors');
+                    });
+                });                
+            });
+        });
+        /*
+        vscode.commands.executeCommand('editor.action.selectHighlights').then(() => {
+            vscode.commands.executeCommand('cursorLeft').then(() => {
+               vscode.commands.executeCommand('extension.align').then(() => {});
+            });
+        });
+        */
     });
 
     context.subscriptions.push(disposable);
